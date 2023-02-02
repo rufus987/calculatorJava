52b83a4148f64038464d521d316bdfd0f25aa83a
rufus987 <65031000+rufus987@users.noreply.github.com>
2023-02-02 22:32:53 +1000

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+/*
+* Да, было бы полезно разбить программу на классы и методы.. наверное
+* 
+*/
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean goCalculator = true;
+        int operand1 = scanner.nextInt();
+
+        while (goCalculator) {
+            char operation = scanner.next().charAt(0);
+            int operand2 = scanner.nextInt();
+            int result = 0;
+            if (operand2 == 0 & operation == '/') {
+                System.out.println("Делить на ноль нельзя!");
+                break;
+            } else {
+                 switch (operation) {
+                     case '+' -> result = operand1 + operand2;
+                     case '-' -> result = operand1 - operand2;
+                     case '*' -> result = operand1 * operand2;
+                     case '/' -> result = operand1 / operand2;
+                     default -> {
+                         System.out.println("Ошибка программы");
+                         System.out.println(operand1);
+                         continue;
+                     }
+                 }
+                System.out.println(result);
+
+                System.out.println("Продолжить?.. Нажмите любую клавишу или \n s чтобы завершить работу калькулятора \n C(англ) чтобы начать сначала");
+                char check = scanner.next().charAt(0);
+
+                if (check == 's'){
+                    System.out.println("Прерывание..");
+                    goCalculator = false;
+                    break;
+                } else if (check == 'C'){
+                    operand1 = 0;
+                    System.out.println(operand1);
+                    continue;
+                }
+                operand1 = result;
+                System.out.println(operand1);
+            }
+        }
+    }
+}
